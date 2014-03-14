829966ef1ec7f8d434542c3d1488a983db4d84f8
Jake Callery <jcallery@subvoicestudios.com>
2014-03-14 17:59:01 -0400

    more minor clean up

--- src/receiver/js/app/background.js
+++ src/receiver/js/app/background.js
@@ -2,7 +2,7 @@
  * Created by Jake on 2/15/14.
  */
 chrome.app.runtime.onLaunched.addListener(function() {
-	chrome.app.window.create('index.html', {
+	chrome.app.window.create('receiver.html', {
 		'bounds': {
 			'width': 800,
 			'height': 600
